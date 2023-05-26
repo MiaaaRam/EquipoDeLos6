ebca500df6a5c5670bf3f2d9d624f06454659d4e
Morales Olivares <321099604@alumno.enp.unam.mx>
2023-05-26 16:53:30 -0600

    feat:Añade inputs para realizar los diferentes movimientos

--- dynamics/login.php
+++ dynamics/login.php
@@ -11,6 +11,12 @@
     echo "<form>
         <fieldset>
         <legend>Movimientos</legend>
+        <label for='mov' >Movimiento:<br>
+                <input type='radio' name='mov' value='Crear archivo' checked>Crear archivo<br>
+                <input type='radio' name='mov' value='Renombrar archivo'>Renombrar archivo<br>
+                <input type='radio' name='mov' value='Eliminar archivo'>Eliminar archivo<br>
+            </label>
+            <br>
         </fieldset>
     </form>
     ";
