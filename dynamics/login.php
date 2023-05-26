06d2a8b741bf3547340616a276a7111d193a61d4
Morales Olivares <321099604@alumno.enp.unam.mx>
2023-05-26 17:22:46 -0600

    style:Cambia fuente de la pagina de inicio

--- dynamics/login.php
+++ dynamics/login.php
@@ -1,6 +1,16 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movimientos</title>
+    <link rel="stylesheet" href="./statics/styles/movimientos.css">
+</head>
+<body>
+    <?php
     $nombre = (isset($_POST["nombre"]) && $_POST["nombre"] !="")? $_POST["nombre"] : "No especifico";
+
 
     echo "<br>Hola " .$_POST["nombre"]. " elige el movimiento que deseas realizar:";
 
@@ -10,15 +20,31 @@
     // action="" method="post" target="_blank" autocomplete="on"
     echo "<form>
         <fieldset>
-        <legend>Movimientos</legend>
-        <label for='mov' >Movimiento:<br>
-                <input type='radio' name='mov' value='Crear archivo' checked>Crear archivo<br>
-                <input type='radio' name='mov' value='Renombrar archivo'>Renombrar archivo<br>
-                <input type='radio' name='mov' value='Eliminar archivo'>Eliminar archivo<br>
+            <label for='mov' >Movimiento:<br>
+                    <input type='radio' name='mov' value='Crear archivo' checked>Crear archivo<br>
+                    <input type='radio' name='mov' value='Renombrar archivo'>Renombrar archivo<br>
+                    <input type='radio' name='mov' value='Eliminar archivo'>Eliminar archivo<br>
             </label>
             <br>
+            <label>
+                <input type='submit' value='Aplicar'>
+            </label>
         </fieldset>
-    </form>
+        </form>
+        <form>
+            <label>
+                <input type='submit' value='Registro'>
+            </label>
+            <br>
+            <br>
+            <label>
+                <input type='submit' value='Salir'>
+            </label>
+        </form>
     ";
 
 ?>
+</body>
+</html>
+
+
