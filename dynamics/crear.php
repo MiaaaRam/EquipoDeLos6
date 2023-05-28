6ae2cb95c8b2197d98fe87d1e3c8df29988fb5bd
MiaaaRam <ramirezrojasmia@gmail.com>
2023-05-27 18:35:59 -0600

    No movi nada, pero no me lo descargaba y lo copie y pegue

---
+++ dynamics/crear.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $arch=(isset($_POST['name']) && $_POST["name"] != "")? $_POST['name'] : "Falta Valor"; //recibe nombre del archivo
+        echo "Hola <br>"; //mensaje de entrada
+        echo $arch."<br>"; //te dice q puso el usuario
+
+        $fp = fopen($arch,"w+"); //lo crea
+        if($fp == false) { 
+            echo "No se ha podido crear el archivo.";  //verifica
+        }
+        // INTETNO DE CAMBIAR LA RUTA DE GUARDADO, NO NECESARIO, PERO QUERÍA INTENTAR
+        // $origen="/".$arch;
+        // $destino="'/archivos/'".$arch;
+        // $cambia_ruta = rename($origen, $destino);
+    ?>
+</body>
+</html>
