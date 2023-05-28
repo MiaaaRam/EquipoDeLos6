536e613dae44d77d654655c9dfcbb4581d65eb77
MiaaaRam <ramirezrojasmia@gmail.com>
2023-05-27 20:38:30 -0600

    Ya renombra archivos (rama)

--- dynamics/renombrar.php
+++ dynamics/renombrar.php
@@ -7,34 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    <form TARGET="_blank" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <fieldset>
-            <legend>Ingresa los siguientes datos para renombrar a tu archivo</legend>
-            <label>Nombre actual del txt (ruta completa con el .txt):
-                <input type="text" name="nombreviejo" id="nombreviejo">
-            </label>
-            <br>
-            <label>Nombre nuevo para el txt(solo nombre con el .txt):
-                <input type="text" name="nombrenuevo" id="nombrenuevo">
-            </label>
-            <br>
-            <br>
-            <label>
-                <input type='submit' value='Enviar'>
-            </label>
-        </fieldset>
-    </form>
+    
     <?php 
         $nviejo = $_POST['nombreviejo'];
         $nnuevo = $_POST['nombrenuevo'];
-        if(isset($_POST['nombre'])) 
+        if(isset($nnuevo)) 
         {
             rename ("$nviejo", "$nnuevo");
-            echo "Listo, vuelve pronto :)"
+            echo "Listo, vuelve pronto :)";
         }
         else
         {
-            echo "Algo a salido mal, intentalo de nuevo y recuerda llenar correctamente el formulario"
+            echo "Algo a salido mal, intentalo de nuevo y recuerda llenar correctamente el formulario";
         }
     ?>
 </body>
